67959218f6597b7ac83a32f3579ce5b8f82e992d
Shenyuan Huang <shenyuan.huang@etu.unice.fr>
2019-01-21 20:11:51 +0100

    #34 ajouter welcome page et selection de visiteur ou guide

--- polymuseum/lib/main.dart
+++ polymuseum/lib/main.dart
@@ -2,18 +2,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:polymuseum/screens/GuideScreen.dart';
-import 'package:polymuseum/screens/VisitorScreen.dart';
-import 'package:polymuseum/sensors/BeaconScanner.dart';
 import 'package:polymuseum/DBHelper.dart';
 import 'package:polymuseum/global.dart' as global;
-import 'package:polymuseum/sensors/BeaconsTool.dart';
 import 'package:polymuseum/sensors/Scanner.dart';
 import 'package:polymuseum/sensors/Gyroscope.dart';
 import 'package:polymuseum/sensors/Accelerometer.dart';
-import 'screens/RaceScreen.dart';
 import 'package:polymuseum/sensors/NFCScanner.dart';
-
+import 'package:polymuseum/welcomePage/ChooseDialog.dart';
+import 'package:polymuseum/welcomePage/SplagePage.dart';
 
 void main() async {
 
@@ -53,43 +49,15 @@ class FirstScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Accueil'),
-      ),
-      body: Center(
-        child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                child: Text('Visiteur'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => VisitorScreen()),
-                  );
-                },
-              ),
-              RaisedButton(
-                child: Text('Guide'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => GuideScreen()),
-                  );
-                },
-              ),RaisedButton(
-                child: Text('Run'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => RaceScreen()),
-                  );
-                },
-              ),
-            ]
-        ),
-      ),
+    return MaterialApp(
+      home: new SplashPage(), 
+      routes: <String, WidgetBuilder>{ 
+      '/HomePage': (BuildContext context) => new Container(
+        color: Colors.grey[200],
+        child: new ChooseDialog(),
+      )
+    },
     );
   }
-}
+}    
+
